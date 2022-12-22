e61f584b7ca14cb116fa316b233a46336b6a501d
dgardiner89 <darrell.gardiner@gmail.com>
2022-12-22 11:08:14 +1100

    Added instagram login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,25 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import Auth0Provider from "next-auth/providers/auth0";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { type NextAuthOptions } from 'next-auth'
+import Auth0Provider from 'next-auth/providers/auth0'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import InstagramProvider from 'next-auth/providers/instagram'
 
-import { env } from "../../../env/server.mjs";
-import { prisma } from "../../../server/db/client";
+import { env } from '../../../env/server.mjs'
+import { prisma } from '../../../server/db/client'
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
+    async signIn({
+      user, account, profile, email, credentials,
+    }: any) {
+      delete account.user_id
+      return true
+    },
     session({ session, user }) {
       if (session.user) {
-        session.user.id = user.id;
+        session.user.id = user.id
       }
-      return session;
+      return session
     },
   },
   adapter: PrismaAdapter(prisma),
@@ -22,7 +29,11 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.AUTH0_CLIENT_SECRET,
       issuer: env.AUTH0_ISSUER,
     }),
+    InstagramProvider({
+      clientId: env.INSTAGRAM_CLIENT_ID,
+      clientSecret: env.INSTAGRAM_CLIENT_SECRET,
+    }),
   ],
-};
+}
 
-export default NextAuth(authOptions);
+export default NextAuth(authOptions)
