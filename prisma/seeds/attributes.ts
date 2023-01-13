c5327128f239aab24eb286b510f07d6c2125ab8d
dgardiner89 <darrell.gardiner@gmail.com>
2023-01-13 13:54:19 +1100

    Added Attribute categories

--- prisma/seeds/attributes.ts
+++ prisma/seeds/attributes.ts
@@ -14,18 +14,32 @@ const attributes: Prisma.AttributeCreateArgs['data'][] = [
     key: 'model_series',
     title: 'Series',
   },
+  {
+    key: 'engine',
+    title: 'Engine',
+  },
+  {
+    key: 'differentials',
+    title: 'Diff',
+  },
+  {
+    key: 'tyre_size',
+    title: 'Tyres',
+  },
+  {
+    key: 'build_goal',
+    title: 'Purpose',
+  },
 ]
 
-const seedFn = (prisma: PrismaClient) => {
-  return attributes.map(async (data: Prisma.AttributeCreateArgs['data']) => {
-    const record = await prisma.attribute.upsert({
-      where: { key: data.key },
-      update: data,
-      create: data,
-    })
-
-    return record
+const seedFn = (prisma: PrismaClient) => attributes.map(async (data: Prisma.AttributeCreateArgs['data']) => {
+  const record = await prisma.attribute.upsert({
+    where: { key: data.key },
+    update: data,
+    create: data,
   })
-}
+
+  return record
+})
 
 export default seedFn
