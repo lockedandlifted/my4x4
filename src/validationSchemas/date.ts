9fc39e672151d4f599bc7832775924737090994e
Ken Greeff <ken@kengreeff.com>
2023-02-02 19:53:23 +1100

    Add ability to add business to part

--- src/validationSchemas/date.ts
+++ src/validationSchemas/date.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod'
 
 export const dateSchema = z.preprocess((arg) => {
-  if (typeof arg === 'string' || arg instanceof Date) {
+  if ((typeof arg === 'string' && arg !== '') || arg instanceof Date) {
     return new Date(arg)
   }
 
   return undefined
-}, z.date())
+}, z.date().optional())
 
 export type DateSchema = z.infer<typeof dateSchema>
--- src/validationSchemas/projectsPart.ts
+++ src/validationSchemas/projectsPart.ts
@@ -5,6 +5,8 @@ export const createProjectsPartValidationSchema = z.object({
   categoryId: z.string(),
   description: z.string().optional(),
   installedAt: z.optional(dateSchema),
+  installedByBusinessId: z.string().optional(),
+  installedByBusinessName: z.string().optional(),
   manufacturerId: z.string(),
   manufacturerPartId: z.string().optional(),
   partNumber: z.string(),
