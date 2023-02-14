2f900b6f1a3497b66685c1541d6e098fe492b8a3
Ken Greeff <ken@kengreeff.com>
2023-02-14 19:41:13 +1100

    Update cache age

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home = () => (
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   res.setHeader(
     'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59',
+    'public, s-maxage=3600, stale-while-revalidate=59',
   )
 
   return { props: {} }
