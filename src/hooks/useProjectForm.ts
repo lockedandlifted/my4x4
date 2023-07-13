0738db73734510d6b8e5c963e4a1e4813b775a18
jacques-initium <jacques@initium.studio>
2023-07-13 10:51:16 +1000

    remove manufacturer model series id from watch and form return

--- src/hooks/useProjectForm.ts
+++ src/hooks/useProjectForm.ts
@@ -179,7 +179,6 @@ function useProjectForm(options: UseProjectFormOptions) {
   const createdByOwner = watch('createdByOwner')
   const manufacturerId = watch('manufacturerId')
   const manufacturerModelId = watch('manufacturerModelId')
-  const manufacturerModelSeriesId = watch('manufacturerModelSeriesId')
   const manufacturerModelSeriesTitle = watch('manufacturerModelSeriesTitle')
   const projectSlug = watch('slug')
   const yearManufactured = watch('attributes.year_manufactured')
@@ -289,7 +288,6 @@ function useProjectForm(options: UseProjectFormOptions) {
     manufacturerId,
     manufacturerModelId,
     manufacturerModels,
-    manufacturerModelSeriesId,
     manufacturerModelSeriesTitle,
     manufacturers,
     mutations: {
