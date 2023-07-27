88df66cbb7502e9dfeda27774fd54e8c10f02199
Ken Greeff <ken@kengreeff.com>
2023-07-27 10:41:17 +1000

    Fix email layout

--- src/jobs/cronMonthlyUnpublishedProjectsEmailsJob.ts
+++ src/jobs/cronMonthlyUnpublishedProjectsEmailsJob.ts
@@ -5,19 +5,22 @@ const cronMonthlyUnpublishedProjectsEmailsJob = inngestClient.createFunction(
   { name: 'Monthly Unpublished Project Emails' },
   { cron: 'TZ=Australia/Sydney 0 17 1 * *' }, // start of month at 5pm
   async ({ step }) => {
-    const usersWithUnpublishedProjects = await step.run('Get Users with Unpublished Projects', async () => prisma.user.findMany({
-      where: {
-        projectsUsers: {
-          some: {
-            project: {
-              createdByOwner: true,
-              notificationsEnabled: true,
-              published: false,
+    const usersWithUnpublishedProjects = await step.run(
+      'Get Users with Unpublished Projects',
+      async () => prisma.user.findMany({
+        where: {
+          projectsUsers: {
+            some: {
+              project: {
+                createdByOwner: true,
+                notificationsEnabled: true,
+                published: false,
+              },
             },
           },
         },
-      },
-    }))
+      }),
+    )
 
     // For each user with unpublished project, send an event
     const events = usersWithUnpublishedProjects.map(user => ({
--- src/mailers/templates/UnpublishedProjectsEmail.tsx
+++ src/mailers/templates/UnpublishedProjectsEmail.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react'
 import {
-  Button, Heading, Img, Section, Text,
+  Button, Column, Heading, Img, Row, Section, Text,
 } from '@react-email/components'
 
 import DefaultLayout from '../layouts/DefaultLayout'
 
-import PlaceHolderImage from '../assets/placeholder.png'
+import PlaceHolderImage from '../assets/project_placeholder.png'
 
 const baseUrl = process.env.VERCEL_URL
   ? 'https://www.my4x4.info'
   : 'http://localhost:3001'
 
-const defaultProjects = [{
-  id: 'cljqfvw7500043b63jz00zphw',
-  slug: 'next-gen-raptor',
-  title: '2023 Ford Ranger Raptor Next Gen',
-  imageUrl: 'https://ik.imagekit.io/lockedandlifted/development/tr:fo-auto,h-200,w-200/images/7d150f97-28dd-44d0-9854-7ba4af307316.png',
-}]
+const defaultProjects = [
+  {
+    id: 'cljqfvw7500043b63jz00zphw',
+    slug: 'next-gen-raptor',
+    title: '2023 Ford Ranger Raptor Next Gen',
+    imageUrl: 'https://ik.imagekit.io/lockedandlifted/development/tr:fo-auto,h-200,w-200/images/7d150f97-28dd-44d0-9854-7ba4af307316.png',
+  },
+  {
+    id: 'cljqfvw7512043b63jz00zphw',
+    slug: '1993-toyota-hilux-l-n106-clisb3fa600063b6mlovufqpd',
+    title: '1993 Toyota Hilux LN106',
+    imageUrl: '',
+  },
+]
 
 type UnpublishedProjectsEmailProps = {
   generationDate: string,
@@ -37,7 +45,8 @@ const UnpublishedProjectsEmail = (props: UnpublishedProjectsEmailProps) => {
     <DefaultLayout
       footerContent={(
         <span>
-          This message was sent to {userEmail}. If you don't want to receive these emails from MY4X4 in the future, you can edit your profile.
+          This message was sent to {userEmail}. If you don't want to receive
+          these emails from MY4X4 in the future, you can edit your profile.
         </span>
       )}
       generationDate={generationDate}
@@ -45,29 +54,35 @@ const UnpublishedProjectsEmail = (props: UnpublishedProjectsEmailProps) => {
     >
       {projects.map(project => (
         <Section className="mt-0" key={project.id}>
-          {!!project.imageUrl && (
-            <Img
-              src={project.imageUrl}
-              width="100"
-              height="100"
-              alt="MY4X4"
-              className="rounded-full mt-4"
-            />
-          )}
+          <Row className="mt-4">
+            <Column style={{ width: '100px' }}>
+              {!!project.imageUrl && (
+                <Img
+                  src={project.imageUrl}
+                  width="100"
+                  height="100"
+                  alt="MY4X4"
+                  className="rounded-full"
+                />
+              )}
 
-          {!project.imageUrl && (
-            <Img
-              src={PlaceHolderImage.src}
-              width="100"
-              height="100"
-              alt="Image Placeholder"
-              className="rounded-full mt-4"
-            />
-          )}
+              {!project.imageUrl && (
+                <Img
+                  src={`${baseUrl}${PlaceHolderImage.src}`}
+                  width="100"
+                  height="100"
+                  alt="Image Placeholder"
+                  className="rounded-full"
+                />
+              )}
+            </Column>
 
-          <Heading className="text-[16px]">
-            {project.title}
-          </Heading>
+            <Column className="pl-4">
+              <Heading className="text-[16px] text-left">
+                {project.title}
+              </Heading>
+            </Column>
+          </Row>
         </Section>
       ))}
 
@@ -77,7 +92,12 @@ const UnpublishedProjectsEmail = (props: UnpublishedProjectsEmailProps) => {
         </Text>
 
         <Text>
-          You're {manyProjects ? "builds aren't" : "build isn't"} published yet, if you've had any problems with publishing please reach out to us and we can help you. Otherwise you can hit the button below to head into your profile and publish the build.
+          The {manyProjects ? "builds above aren't" : "build above isn't"} published yet.
+          If you've had problems with publishing please reach out to us so we can help you.
+        </Text>
+
+        <Text>
+          Otherwise you can hit the button below to head to your profile and publish the build.
         </Text>
       </Section>
 
@@ -86,7 +106,7 @@ const UnpublishedProjectsEmail = (props: UnpublishedProjectsEmailProps) => {
           className="bg-[#000000] rounded text-white text-[16px] mt-4 py-4 px-8"
           href={`${baseUrl}/users/account`}
         >
-          Publish
+          Manage {manyProjects ? 'Builds' : 'Build'}
         </Button>
       </Section>
     </DefaultLayout>
